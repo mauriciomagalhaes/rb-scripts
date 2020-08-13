0c9a8dd439dfe89a7005c4a1354137c320f580c7
root <root@MAURICIO-G3.localdomain>
2020-08-13 14:01:51 -0300

    .

--- change-route-pb.py
+++ change-route-pb.py
@@ -10,7 +10,7 @@ import time
 conn = routeros_api.RouterOsApiPool(
     '10.4.1.1', 
     username='admin', 
-    password='agora100sempre',
+    password='passwd',
     plaintext_login=True
     )
 
@@ -21,12 +21,21 @@ lista = api.get_resource('/ip/route')
 ident = (lista.get(comment='ZIMBRA')[0]['id'])
 stats = (lista.get(comment='ZIMBRA')[0]['disabled'])
 
+if stats == 'true':
+    point = ['T3 Dedicado **','Vivo Adsl','Sair'] 
+else:
+    point = ['T3 Dedicado','Vivo Adsl <=','Sair'] 
+    
 while True:
-    resposta = menu(['T3 Dedicado','Vivo Adsl','Sair'])
+        
+    resposta = menu(point)
+    
     if resposta == 1:
          lista.set(id=ident, disabled="true")
+         point = ['T3 Dedicado *','Vivo Adsl','Sair']
     elif resposta == 2:
          lista.set(id=ident, disabled="false")
+         point = ['T3 Dedicado','Vivo Adsl *','Sair'] 
     elif resposta == 3:
         print('Saindo do Sistema ...')
         time.sleep(3)
